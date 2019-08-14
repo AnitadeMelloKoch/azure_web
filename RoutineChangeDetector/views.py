a0f129e6f164fe24ff917c37e27edde40c81d3c8
Nicholas Kastanos <nicholaskastanos@gmail.com>
2019-08-14 13:38:15 +0200

    Testing a post call

--- RoutineChangeDetector/views.py
+++ RoutineChangeDetector/views.py
@@ -6,7 +6,12 @@ from .models import UserData, UserRoutine
 def index(request):
     # print(request.POST['id'])
     return HttpResponse("Hello, world. You're at the RoutineChangeDetector index.")
+
 def other(request):
     c = UserData.objects.all().count()
     print(c)
     return HttpResponse("This is another index:\n There are " + str(c) + " records" )
+
+def posttest(request):
+    print(request.POST)
+    return HttpResponse(request.POST['data'])
