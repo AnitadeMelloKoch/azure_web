9721fcd6d3f0e5e6e318e1d18900d219e3c36993
Nicholas Kastanos <nicholaskastanos@gmail.com>
2019-08-20 13:51:53 +0200

    more static roots

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static')
 print(STATIC_URL)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 print(STATIC_ROOT)
