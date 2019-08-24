25a959cb071f023667a5731cd41f33d60a10bcc9
Nicholas Kastanos <nicholaskastanos@gmail.com>
2019-08-24 14:34:05 +0200

    Lots of changes

--- RoutineChangeDetector/urls.py
+++ RoutineChangeDetector/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('data/', views.user_data_list, name='get_user_data_list'),
     path('predict-actions/', views.predict_actions, name='predict_actions'),
-    path('detect-anomalies/', views.detect_anomalies, name='detect_anomalies')
+    path('detect-anomalies/', views.detect_anomalies, name='detect_anomalies'),
+    path('get-anomalies/', views.get_user_anomalies, name='get_user_anomalies'),
+    path('get-num-user-records/', views.get_user_record_num, name='get_num_user_records'),
 ]
