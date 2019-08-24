7d3accc929d402f85f61d9fcdfc4b3b41891aad7
Anita <demellokochanita@gmail.com>
2019-08-24 14:39:24 +0200

    Fixed inference script

--- RoutineChangeDetector/inference/inferenceMLP.py
+++ RoutineChangeDetector/inference/inferenceMLP.py
@@ -29,18 +29,16 @@ def predict(data, meta_dir, checkpoint_dir, validate):
         bias_5 = graph.get_tensor_by_name("weight_5_bias:0")
         weight_6 = graph.get_tensor_by_name("weight_6:0")
         bias_6 = graph.get_tensor_by_name("weight_6_bias:0")
-        weight_action_1_layer_1 = graph.get_tensor_by_name("weight_action_1_layer_1:0")
-        bias_action_1_layer_1 = graph.get_tensor_by_name("weight_action_1_layer_1_bias:0")
         weight_action_2_layer_1 = graph.get_tensor_by_name("weight_action_2_layer_1:0")
         bias_action_2_layer_1 = graph.get_tensor_by_name("weight_action_2_layer_1_bias:0")
+        weight_loc_layer_1 = graph.get_tensor_by_name("loc_layer_1:0")
+        bias_loc_layer_1 = graph.get_tensor_by_name("loc_layer_1_bias")
         weight_out_action_1 = graph.get_tensor_by_name("weight_out_action_1:0")
         bias_out_action_1 = graph.get_tensor_by_name("weight_out_action_1_bias:0")
         weight_out_action_2 = graph.get_tensor_by_name("weight_out_action_2:0")
         bias_out_action_2 = graph.get_tensor_by_name("weight_out_action_2_bias:0")
         weight_out_loc = graph.get_tensor_by_name("weight_out_loc:0")
         bias_out_loc = graph.get_tensor_by_name("weight_out_loc_bias:0")
-        weight_out_phone = graph.get_tensor_by_name("weight_out_phone:0")
-        bias_out_phone = graph.get_tensor_by_name("weight_out_phone_bias:0")
 
         # initialize all layers
         layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weight_1), bias_1))
@@ -49,14 +47,13 @@ def predict(data, meta_dir, checkpoint_dir, validate):
         layer_4 = tf.nn.sigmoid(tf.add(tf.matmul(layer_3, weight_4), bias_4))
         layer_5 = tf.nn.sigmoid(tf.add(tf.matmul(layer_4, weight_5), bias_5))
         layer_6 = tf.nn.sigmoid(tf.add(tf.matmul(layer_5, weight_6), bias_6))
-        layer_action_1_layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(layer_6, weight_action_1_layer_1), bias_action_1_layer_1))
         layer_action_2_layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(layer_6, weight_action_2_layer_1), bias_action_2_layer_1))
+        layer_loc_layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(layer_6, weight_loc_layer_1), bias_loc_layer_1))
 
         # initialize predictions
-        predicted_action_1 = tf.sigmoid(tf.add(tf.matmul(layer_action_1_layer_1, weight_out_action_1), bias_out_action_1))
+        predicted_action_1 = tf.sigmoid(tf.add(tf.matmul(layer_5, weight_out_action_1), bias_out_action_1))
         predicted_action_2 = tf.sigmoid(tf.add(tf.matmul(layer_action_2_layer_1, weight_out_action_2), bias_out_action_2))
-        predicted_loc = tf.sigmoid(tf.add(tf.matmul(layer_6, weight_out_loc), bias_out_loc))
-        predicted_phone = tf.sigmoid(tf.add(tf.matmul(layer_6, weight_out_phone), bias_out_phone))
+        predicted_loc = tf.sigmoid(tf.add(tf.matmul(layer_loc_layer_1, weight_out_loc), bias_out_loc))
 
         data_in = np.asarray(data)
 
@@ -64,11 +61,6 @@ def predict(data, meta_dir, checkpoint_dir, validate):
         action_1 = sess.run(predicted_action_1, feed_dict={x:data_in})
         action_2 = sess.run(predicted_action_2, feed_dict={x:data_in})
         loc = sess.run(predicted_loc, feed_dict={x:data_in})
-        phone = sess.run(predicted_phone, feed_dict={x:data_in})
-        # print(action_1)
-        # print(action_2)
-        # print(loc)
-        # print(phone)
 
         output = np.concatenate((action_1, action_2, loc), axis=1)
 
